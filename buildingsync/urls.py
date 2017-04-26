4f8c6ed2540234f4ffd407e0df39360845ec3d5f
Edwin Lee <leeed2001@gmail.com>
2017-04-26 14:14:10 -0600

    Add initial testing support

--- buildingsync/urls.py
+++ buildingsync/urls.py
@@ -4,7 +4,7 @@ from .views import index, get_schema, get_use_cases
 
 app_name = 'buildingsync'
 urlpatterns = [
-    url(r'^$', index),
+    url(r'^$', index, name='index'),
     url(r'^get_schema/$', get_schema, name='get_schema'),
     url(r'^get_use_cases/$', get_use_cases, name='get_use_cases'),
 ]
