30475fbe5a2ca1176dd1513feb4ddb861c20ae7b
Edwin Lee <leeed2001@gmail.com>
2017-05-02 07:44:15 -0600

    Add link to admin page

--- main/urls.py
+++ main/urls.py
@@ -17,7 +17,7 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url(r'^admin/', admin.site.urls, name='admin'),
     url(r'^', include('buildingsync.urls', namespace='buildingsync')),
     url(r'^accounts/', include('django.contrib.auth.urls')),
 ]
