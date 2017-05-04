6ad487f2bd0c2e8be31276c60e0dff3d2e71b24b
Edwin Lee <leeed2001@gmail.com>
2017-05-04 16:34:58 -0600

    Add coverage for attribute use case api views

--- buildingsync/views.py
+++ buildingsync/views.py
@@ -60,17 +60,19 @@ class BuildingSyncAttributeViewSet(viewsets.ModelViewSet):
     def remove_use_case(self, request, pk):
         use_case_num = request.data['use_case_num']
         this_attribute = BuildingSyncAttribute.objects.get(pk=pk)
-        if any([uc.id == use_case_num for uc in this_attribute.use_cases.all()]):
+        if any([str(uc.id) == use_case_num for uc in this_attribute.use_cases.all()]):
             this_attribute.use_cases.remove(use_case_num)
+            return JsonResponse({'status': 'success'})
         else:
-            pass  # error
+            return JsonResponse({'status': 'error'}, status=status.HTTP_400_BAD_REQUEST)
 
     @decorators.detail_route(methods=['PUT'])
     def add_use_case(self, request, pk):
         use_case_num = request.data['use_case_num']
         this_attribute = BuildingSyncAttribute.objects.get(pk=pk)
         this_use_case = UseCase.objects.get(pk=use_case_num)
-        if any([uc.id == use_case_num for uc in this_attribute.use_cases.all()]):
-            pass  # error
+        if any([str(uc.id) == use_case_num for uc in this_attribute.use_cases.all()]):
+            return JsonResponse({'status': 'error'}, status=status.HTTP_400_BAD_REQUEST)
         else:
             this_attribute.use_cases.add(this_use_case)
+            return JsonResponse({'status': 'success'})
