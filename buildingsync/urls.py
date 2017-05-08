94e27a671d6991926ade6f43bdd71c0223c7b540
Edwin Lee <leeed2001@gmail.com>
2017-05-08 05:16:28 -0600

    OK, done refactoring

--- buildingsync/urls.py
+++ buildingsync/urls.py
@@ -1,12 +1,12 @@
 from django.conf.urls import url, include
-from buildingsync.views import main, buildingsync_attributes, schemas, use_cases, users
+from buildingsync.views import main, attributes, schemas, use_cases, users
 from rest_framework_swagger.views import get_swagger_view
 from rest_framework import routers
 
 api_router = routers.SimpleRouter()
 api_router.register(r'schemas', schemas.SchemaViewSet, base_name='schemas')
 api_router.register(r'use_cases', use_cases.UseCaseViewSet, base_name='use_cases')
-api_router.register(r'attributes', buildingsync_attributes.BuildingSyncAttributeViewSet, base_name='attributes')
+api_router.register(r'attributes', attributes.BuildingSyncAttributeViewSet, base_name='attributes')
 api_router.register(r'current_user_id', users.CurrentUserViewSet, base_name='current_user_id')
 
 
