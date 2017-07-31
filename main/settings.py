ba61ba6d39b0f3707c0ee23f853810d09b605df4
Nicholas Long <nicholas.long@nrel.gov>
2017-07-31 11:00:45 -0600

    add allowed host

--- main/settings.py
+++ main/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'ggo+u$&s&adrgss!rufpisb*9t^mt)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
-ALLOWED_HOSTS = ['buildingsynctool.herokuapp.com', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['selectiontool.buildingsync.net', '127.0.0.1', 'localhost']
 
 # Application definition
 
