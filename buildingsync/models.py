8d77c9b3b6ceb4810351b1aecbb7a6b78b8224c4
Nicholas Long <nicholas.long@nrel.gov>
2017-08-02 13:15:56 -0600

    longer length in path

--- buildingsync/models.py
+++ buildingsync/models.py
@@ -25,4 +25,4 @@ class BuildingSyncAttribute(models.Model):
     ignored_use_cases = models.ManyToManyField(UseCase, related_name="ignored_use_cases")
     tree_level = models.IntegerField()
     index = models.IntegerField(verbose_name="For a given schema, this is the linear index in the tree list", default=0)
-    path = models.CharField(max_length=250, default="")
+    path = models.CharField(max_length=500, default="")
