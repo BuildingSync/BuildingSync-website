1df86e4aaaa3a3881b958fe9fcd627f8b7a1c4de
kflemin <katherine.fleming@nrel.gov>
2018-08-27 21:36:02 -0600

    login, profile, logout

--- bsviewer/views.py
+++ bsviewer/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from django.contrib.auth.decorators import login_required, permission_required, user_passes_test
+
 
 
 def index(request):
     context = {}
     return render(request, 'bsviewer/index.html', context)
+
+@login_required
+def profile(request):
+    if request.GET and request.GET['passwordchange'] == 'done':
+        messages.add_message(request, messages.SUCCESS, 'Password changed')
+    return render(request, 'registration/profile.html')
+
 
--- bswebapp/settings.py
+++ bswebapp/settings.py
@@ -118,6 +118,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+LOGIN_REDIRECT_URL = '/viewers'
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
