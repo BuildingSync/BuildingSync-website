b5548f8382419afcd56b72968d3bb1b4a362f383
kflemin <katherine.fleming@nrel.gov>
2018-10-11 16:13:58 -0600

    validate version format to be x.x.x on schema admin form

--- bsviewer/admin.py
+++ bsviewer/admin.py
@@ -1,15 +1,33 @@
+import re
 from django.contrib import admin
 from django.contrib.admin import AdminSite
 from django.contrib.auth.admin import GroupAdmin, UserAdmin
 from django.contrib.auth.models import Group, User
+from django import forms
 
 from .models.attribute import Attribute
 from .models.schema import Schema
 from .models.use_case import UseCase
 
 
+class SchemaForm(forms.ModelForm):
+    class Meta:
+        model = Schema
+        fields = '__all__'
+
+    # validate that schema version is in X.X.X format (3 parts)
+    def clean(self):
+        version = self.cleaned_data.get('version')
+        pattern = re.compile("\d+\.\d+\.\d+")
+
+        if not pattern.match(version):
+            raise forms.ValidationError("Version must be in X.X.X format. Example: 0.3.0.")
+
+        return self.cleaned_data
+
+
 class SchemaAdmin(admin.ModelAdmin):
-    list_display = ('name', 'version', 'schema_file', 'schema_parsed')
+    form = SchemaForm
 
 
 class UseCaseAdmin(admin.ModelAdmin):
