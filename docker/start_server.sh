df58dcb5ab81004aaa625d7f4c6d314ecfa4c5b6
Nicholas Long <nicholas.long@nrel.gov>
2018-10-11 14:39:40 -0600

    add loading bedes when launching site in docker

--- docker/start_server.sh
+++ docker/start_server.sh
@@ -11,6 +11,7 @@ echo "Waiting for postgres to start"
 
 # Check if a schema has been imports
 ./manage.py reset_schema --schema_version 0.3.0
+./manage.py bedes --save_to_db
 
 # Create the default user based on the env vars
 #./manage.py createsuperuser ${SELECTION_TOOL_ADMIN_USER} ${SELECTION_TOOL_ADMIN_EMAIL} ${SELECTION_TOOL_ADMIN_PASSWORD}
