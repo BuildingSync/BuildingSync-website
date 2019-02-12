f0bdd99aec8e4013ffa98855a942a25727b3b798
kflemin <katherine.fleming@nrel.gov>
2019-02-12 13:22:57 -0700

    update docker script to 1.0.0

--- docker/start_server.sh
+++ docker/start_server.sh
@@ -10,8 +10,8 @@ echo "Waiting for postgres to start"
 ./manage.py collectstatic --noinput
 
 # Check if a schema has been imports
-./manage.py reset_schema --schema_version 0.3.0
-./manage.py bedes --schema_version=0.3.0 --bedes_version=v2.2 --save_to_db
+./manage.py reset_schema --schema_version 1.0.0
+./manage.py bedes --schema_version=1.0.0 --bedes_version=v2.2 --save_to_db
 
 # Create the default user based on the env vars
 echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('${SELECTION_TOOL_ADMIN_USER}', '${SELECTION_TOOL_ADMIN_EMAIL}', '${SELECTION_TOOL_ADMIN_PASSWORD}')" | python manage.py shell
