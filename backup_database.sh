ae86f14933be5ba352be8b8699d0feb0694e6c51
Nicholas Long <nicholas.long@nrel.gov>
2020-12-18 11:38:29 -0700

    typo on media

--- backup_database.sh
+++ backup_database.sh
@@ -37,7 +37,7 @@ docker exec $(docker ps -f "name=db-postgres" --format "{{.ID}}") pg_dump -U ${D
 # Backup the media directory (uploads, especially buildingsync). In docker-land this is
 # just a container volume, so create a new container with the volume attached and tar it up.
 echo "docker run --rm -it -v selectiontool_mediadata:/backup/media -v $BACKUP_DIR:/backup/dir/ alpine:3.8 tar zcvf $(media_file_name) /backup/media"
-docker run --rm -v selectiontool_media:/backup/media -v $BACKUP_DIR:/backup/dir/ alpine:3.8 tar zcvf $(media_file_name) /backup/media
+docker run --rm -v selectiontool_mediadata:/backup/media -v $BACKUP_DIR:/backup/dir/ alpine:3.8 tar zcvf $(media_file_name) /backup/media
 
 # Delete files older than 30 days.
 find ${BACKUP_DIR} -mtime +30 -type f -name '*.dump' -delete
