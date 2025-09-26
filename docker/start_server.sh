8bb86ec5ce7b86647b53bf3024a3de6ec244f1a6
Nicholas Long <nicholas.long@nrel.gov>
2025-09-26 15:30:41 -0600

    update docker container for python 3.9+

--- docker/start_server.sh
+++ docker/start_server.sh
@@ -10,8 +10,8 @@ echo "Waiting for postgres to start"
 ./manage.py collectstatic --noinput
 
 # Check if a schema has been imported
-./manage.py reset_schema --schema_version 1.0.0
-./manage.py bedes --schema_version=1.0.0 --bedes_version=v2.2 --save_to_db
+./manage.py reset_schema --schema_version 2.6.0
+./manage.py bedes --schema_version=2.6.0 --bedes_version=v2.5 --save_to_db
 
 # Create the default user based on the env vars
 echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('${BUILDINGSYNC_WEBSITE_ADMIN_USER}', '${BUILDINGSYNC_WEBSITE_ADMIN_EMAIL}', '${BUILDINGSYNC_WEBSITE_ADMIN_PASSWORD}')" | python manage.py shell
