f4f9903043a27b28c9d0b1e36cce3dac97f4fa61
Katherine Fleming <2205659+kflemin@users.noreply.github.com>
2025-10-10 10:02:46 -0600

    Django update (#212)
    * language update
    
    * update to django 3
    
    * precommit
    
    * downgrade psycopg
    
    ---------
    
    Co-authored-by: Nicholas Long <nicholas.long@nrel.gov>
    Co-authored-by: Nicholas Long <1907354+nllong@users.noreply.github.com>

--- bsyncviewer/settings/docker.py
+++ bsyncviewer/settings/docker.py
@@ -151,6 +151,9 @@ MEDIA_ROOT = os.path.join(SETTINGS_PATH, "media")
 
 DEFAULT_SCHEMA_VERSION = "2.7.0"
 
+# Default primary key field type for Django 3.2+
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
 with open(
     os.path.join(os.path.dirname(os.path.dirname(__file__)), "../app_version.txt")
 ) as v_file:
